fdfbb4da8a17475bc47f12f6b775ab3fa0e8369b
fabio <fabione2005@gmail.com>
2024-07-26 18:23:19 -0400

    first commit

---
+++ src/test/java/com/fub/springboot/db/wikimedia/WikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fub.springboot.db.wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
